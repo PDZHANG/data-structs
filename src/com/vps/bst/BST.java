a9385175df7bc057f712319080122594ca366b67
allen.zhang <1947098945@qq.com>
2019-06-06 16:46:57 +0800

    添加BST中序，后序遍历

--- src/com/vps/bst/BST.java
+++ src/com/vps/bst/BST.java
@@ -120,6 +120,42 @@ public class BST<E extends Comparable<E>> {
         preOrder(node.right);
     }
 
+
+    // 中序搜索树的中序遍历
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    // 中序遍历以node为根的二分搜索树, 递归算法
+    private void inOrder(Node node){
+        if(node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+
+    // 二分搜索树的后序遍历
+    public void afterOrder(){
+        afterOrder(root);
+    }
+
+    // 后序遍历以node为根的二分搜索树, 递归算法
+    private void afterOrder(Node node){
+        if(node == null) {
+            return;
+        }
+        afterOrder(node.left);
+        afterOrder(node.right);
+        System.out.println(node.e);
+    }
+
+
+
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
